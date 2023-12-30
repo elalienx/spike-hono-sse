1787db747dff975395427d692ebfe0c18ba676ed
Eduardo <el_alienx@hotmail.com>
2023-12-30 17:38:38 +0100

    modified header css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="header">
-      <h1>This is a multicontainer application</h1>
+      <h1>This is a multi container Docker application 🐳</h1>
       <Link to="/">Home</Link>
       <Link to="/other">Other page</Link>
     </header>
