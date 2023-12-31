4c6ebd4cc3f05ad7cbd03cc4f6e6635d1f25ec74
Eduardo <el_alienx@hotmail.com>
2023-12-31 02:33:15 +0100

    refactor client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,54 @@
 // Node modules
-import { BrowserRouter, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 // Project files
-import Header from "./components/Header";
-import Other from "./pages/Other";
-import Home from "./pages/Home";
 import "./styles/style.css";
 
 export default function App() {
+  // Local state
+  const [data, setData] = useState([]);
+  const [licence, setLicence] = useState("");
+
+  // Properties
+  const endpoint = "/api/values/all"; // 🔔 IMPORTANT: We will use nginx to redirect it to the proper URL
+
+  // Methods
+  useEffect(() => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((result) => setData(result.rows.map((row) => row.number)));
+  }, []);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    await axios.post("/api/values", { value: licence });
+
+    setLicence("");
+    setData(...data, licence);
+  }
+
+  // Components
+  const Items = data.map((item, index) => (
+    <div key={index} className="item">
+      SWE-{item}
+    </div>
+  ));
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Route exact path="/" component={Home} />
-        <Route path="/other" component={Other} />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <h1 className="title">Licence Plates 🚗</h1>
+      {Items}
+      <hr />
+      <form className="form" onSubmit={(event) => onSubmit(event)}>
+        <label>Register new car (only numbers):</label>
+        <input
+          value={licence}
+          onChange={(event) => setLicence(event.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 }
